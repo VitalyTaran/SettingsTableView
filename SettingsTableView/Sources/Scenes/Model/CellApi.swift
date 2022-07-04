3827a8e6f8e590bcc63caafb771ee4ebb021e5ca
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-07-04 19:20:29 +0300

    bug fix - MVC pattern

---
+++ SettingsTableView/Sources/Scenes/Model/CellApi.swift
@@ -0,0 +1,69 @@
+//
+//  CellApi.swift
+//  SettingsTableView
+//
+//  Created by Виталий Таран on 04.07.2022.
+//
+
+
+import UIKit
+
+struct CellApi {
+    func getData() -> [[Cell]] {
+        let section1 = [Cell(image: "airplane",
+                             iconColor: .systemOrange,
+                             title: "Авиарежим",
+                             isToggle: true),
+                        Cell(image: "wifi",
+                             title: "Wi-Fi",
+                             detail: "Не подключено"),
+                        Cell(image: "bluetooth",
+                             title: "Bluetooth",
+                             detail: "Вкл.",
+                             isCustomCell: true),
+                        Cell(image: "antenna.radiowaves.left.and.right",
+                             iconColor: .systemGreen,
+                             title: "Сотовая связь"),
+                        Cell(image: "personalhotspot",
+                             iconColor: .systemGreen,
+                             title: "Режим модема",
+                             detail: "Выкл."),
+                        Cell(image: "vpn",
+                             title: "VPN",
+                             isToggle: true,
+                             isCustomCell: true)]
+
+        let section2 = [Cell(image: "bell.badge.fill",
+                             iconColor: .systemRed,
+                             title: "Уведомления"),
+                        Cell(image: "speaker.wave.3.fill",
+                             iconColor: .systemRed,
+                             title: "Звуки, тактильные сигналы"),
+                        Cell(image: "moon.fill",
+                             iconColor: .systemIndigo,
+                             title: "Не беспокоить"),
+                        Cell(image: "hourglass",
+                             iconColor: .systemIndigo,
+                             title: "Экранное время")]
+
+        let section3 = [Cell(image: "gear",
+                             iconColor: .systemGray,
+                             title: "Основные",
+                             badge: 1),
+                        Cell(image: "switch.2",
+                             iconColor: .systemGray,
+                             title: "Пункт управления"),
+                        Cell(image: "textformat.size",
+                             title: "Экран и яркость"),
+                        Cell(image: "homescreen",
+                             iconColor: .systemBlue,
+                             title: "Экран \u{00AB}Домой\u{00BB}",
+                             isCustomCell: true),
+                        Cell(image: "accessibility",
+                             title: "Универсальный доступ",
+                             isCustomCell: true)]
+
+        return [section1, section2, section3]
+    }
+}
+
---
+++ SettingsTableView/Sources/Scenes/Model/Metric.swift
@@ -0,0 +1,18 @@
+//
+//  Metric.swift
+//  SettingsTableView
+//
+//  Created by Виталий Таран on 04.07.2022.
+//
+
+import UIKit
+
+enum Metric {
+
+    static let navigationTitle: String = "Настройки"
+    static let iconColorViewSize: CGFloat = 30
+    static let iconColorViewCornerRadius: CGFloat = 6
+    static let defaultCelliConSymbolSize: CGFloat = 21
+    static let customCelliConSymbolSize: CGFloat = 22
+}
+
